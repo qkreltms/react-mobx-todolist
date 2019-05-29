3b74550864410209f03aca6cd6ddb7aabb74919f
wfr112@naver.com <wfr112@naver.com>
2019-05-29 20:26:21 +0900

    update: todo have done

--- src/js/TodoList.jsx
+++ src/js/TodoList.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import { observer } from "mobx-react"
+import { toUnicode } from 'punycode';
 
 @observer
 class TodoList extends React.Component {
+    filter = (e) => {
+        this.props.store.filter = e.target.value
+    }
+
+    pushTodo = (e) => {
+        if (e.key === 'Enter' && e.target.value) {
+            this.props.store.pushTodo(e.target.value)
+            e.target.value = ""
+        }
+    }
+
+    toggleComplete = (todo) => (e) => {
+        todo.isComplete = !todo.isComplete
+    }
+
     render() {
+        const { filter, filteredTodos } = this.props.store
+        const todoList = filteredTodos.map(todo => (
+            <li key={todo.id}>
+                <input type="checkbox" value={todo.isComplete} checked={todo.isComplete} onChange={this.toggleComplete(todo)} />
+                {todo.value}
+            </li>
+        ))
+
         return (
-            <div>{this.props.store.todos[0]}</div>
+            <div>
+                <h1>todos</h1>
+                <input onKeyPress={this.pushTodo} />
+                Searching
+                <input className="filter" value={filter} onChange={this.filter} />
+                {filter}
+                <ul>{todoList}</ul>
+                <button onClick={this.props.store.clearCompleteTodos}>Clear Complete</button>
+            </div>
         )
     };
 }
--- src/js/TodoStore.jsx
+++ src/js/TodoStore.jsx
@@ -1,15 +1,42 @@
-import { observable, autorun } from 'mobx'
+import { computed, observable, autorun, values } from 'mobx'
 
-class TodoStore {
-    @observable todos = ['buy milk', 'buy eggs']
-    @observable filter = ""
+// Object
+class Todo {
+    @observable value
+    @observable id
+    @observable isComplete
+
+    constructor(value = "") {
+        this.value = value
+        this.id = Date.now()
+        this.isComplete = false
+    }
 }
 
-const store = window.store = new TodoStore
+class TodoStore {
+    @observable todos = []
+    @observable filter = ""
+    // filter에 따라서 정규식 통과한 todo 만 가져옴
+    @computed get filteredTodos() {
+        const matchesFilter = new RegExp(this.filter, "i") // 대소문자 구분안함
+        return this.todos.filter(todo => matchesFilter.test(todo.value))
+    }
 
-export default store
+    pushTodo = (todo) => {
+        this.todos.push(new Todo(todo))
+    }
 
-autorun(() => {
-    console.log(store.filter)
-    console.log(store.todos[0])
-})
+    clearCompleteTodos = () => {
+        const incompleteTodos = this.todos.filter(todo => !todo.isComplete)
+        this.todos = [...incompleteTodos]
+    }
+}
+
+// const store = window.store = new TodoStore
+
+export default new TodoStore
+
+// autorun(() => {
+//     console.log(store.filter)
+//     console.log(store.todos[0])
+// })
